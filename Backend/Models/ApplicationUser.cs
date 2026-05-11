f3e48a0a25f819ffad7eb34c6af4b926dcf7b644
johnsamy7 <167789936+johnsamy7@users.noreply.github.com>
2026-05-11 22:18:47 +0300

    MT-21-Task-1-Design-User branch

--- Backend/Models/ApplicationUser.cs
+++ Backend/Models/ApplicationUser.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Identity;
 using RentVibe.Models.Enums;
 
@@ -5,6 +6,19 @@ namespace RentVibe.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    public string FullName { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; } = UserRole.Tenant;
+
+    public AccountStatus AccountStatus { get; set; } = AccountStatus.Approved;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     
-    
+    public ICollection<Property> Properties { get; set; } = new List<Property>();
+    public ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
+    public ICollection<VisitAppointment> VisitAppointments { get; set; } = new List<VisitAppointment>();
+    public ICollection<RentalApplication> RentalApplications { get; set; } = new List<RentalApplication>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
 }
